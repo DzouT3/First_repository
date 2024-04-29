2f24c584f384ef02e2b81cee219396a5bc4611af
DzouT3 <v.vorontsov.mail@gmail.com>
2024-04-29 17:37:00 +0300

    updatedHomework

--- homework1module3VV.py
+++ homework1module3VV.py
@@ -1,16 +1,14 @@
 from datetime import datetime
 def get_days_from_today(date):
-    ### Ввод данных
-    string_date= (input("Input date: "))
+    ### Параметр с примером
+    string_date= ("2020-10-09")
     ### Инструкция по обработке вводимых данных
     try:
         datetime_date= datetime.strptime(string_date, "%Y-%m-%d").date()
         date_today= datetime.now().date()
         number_day= date_today- datetime_date
-        return(print(f"Number of days:", number_day.days))
+        return number_day.days
     ### Проверка на неправильный ввод данных
     except ValueError:
         print("Please input correct date in format: Year-month-day")
-        return get_days_from_today("date")
-    ### Пример ввода и вывода результата
-print(get_days_from_today("date"))
+        return
