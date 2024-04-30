b12eee4f94c289317939c1829e6d6859a2eb4d69
DzouT3 <v.vorontsov.mail@gmail.com>
2024-04-30 15:01:50 +0300

    updated

--- homework1module3.py
+++ homework1module3.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 def get_days_from_today(date):
-    ### Параметр с примером
-    string_date= ("2020-10-09")
+   
     ### Инструкция по обработке вводимых данных
     try:
-        datetime_date= datetime.strptime(string_date, "%Y-%m-%d").date()
+        datetime_date= datetime.strptime(date, "%Y-%m-%d").date()
         date_today= datetime.now().date()
         number_day= date_today- datetime_date
         return number_day.days
--- homework2module3.py
+++ homework2module3.py
@@ -1,10 +1,12 @@
 from random import randint, sample
 def get_numbers_ticket(min, max, quantity):
     ### Инструкция по обработке рандома в диапазоне заданных значений
-    min<=quantity<=max
+    
     lottery_numbers = set()
 
     if 0<min<max<=1000:
+        if min>max:
+            return lottery_numbers==""
         while len(lottery_numbers) != quantity:
             lottery_numbers.add(randint(min, max))
             return(sorted(sample(range(min, max), quantity)))
