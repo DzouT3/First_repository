11cd76af3aa1fc3a5ee26595189e5f2470717ab2
DzouT3 <v.vorontsov.mail@gmail.com>
2024-05-01 16:33:04 +0300

    update

--- homework2module3.py
+++ homework2module3.py
@@ -7,6 +7,8 @@ def get_numbers_ticket(min, max, quantity):
     if 0<min<max<=1000:
         if min>max:
             return lottery_numbers==""
+        if quantity > max - min + 1:
+            return lottery_numbers==""
         while len(lottery_numbers) != quantity:
             lottery_numbers.add(randint(min, max))
             return(sorted(sample(range(min, max), quantity)))
