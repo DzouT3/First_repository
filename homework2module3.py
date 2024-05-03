b81181ac3b6e2327b3a2de5a70677240f4018d0a
DzouT3 <v.vorontsov.mail@gmail.com>
2024-05-03 15:07:18 +0300

    update

--- homework2module3.py
+++ homework2module3.py
@@ -1,15 +1,11 @@
 from random import randint, sample
 def get_numbers_ticket(min, max, quantity):
-    ### Инструкция по обработке рандома в диапазоне заданных значений
-    
-    lottery_numbers = set()
-
-    if 0<min<max<=1000:
-        if min>max:
-            return lottery_numbers==""
-        if quantity > max - min + 1:
-            return lottery_numbers==""
-        while len(lottery_numbers) != quantity:
-            lottery_numbers.add(randint(min, max))
-            return(sorted(sample(range(min, max), quantity)))
-    else: ValueError
+    # Проверка корректности заданых значений
+    if 0 < min < max <= 1000:
+        if 0 < quantity <= max - min + 1:
+            # Генерация уникальных случайных чисел
+            lottery_numbers = set()
+            while len(lottery_numbers) < quantity:
+                lottery_numbers.add(randint(min, max))
+            return sorted(sample(list(lottery_numbers), quantity))
+    return []
